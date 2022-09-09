d6be81becf50c8fec01d45d533de3043d92c3c20
CarolinaOrellana <caryorellanam@gmail.com>
2022-09-09 16:43:43 -0300

    tarea completada

---
+++ cuenta.py
@@ -0,0 +1,44 @@
+#Crea una clase CuentaBancaria con los atributos tasa de interés y balance
+
+class CuentaBancaria:
+    #ATRIBUTOS
+    todas_las_cuentas = []
+    #parametros predeterminados 
+    def __init__ (self,balance=0,tasa_interes=0.01):
+        self.balance = balance
+        self.tasa_interes= tasa_interes
+        CuentaBancaria.todas_las_cuentas.append(self)
+
+    # Agrega un método depósito a la clase CuentaBancaria:
+    def deposito(self,monto):
+        self.balance += monto
+        return self
+
+    #Agrega un método retiro a la clase CuentaBancaria:
+    def retiro(self,monto):
+        if self.balance>monto:
+            self.balance -= monto
+        else:
+            print (f"Fondos insuficientes: cobrando una tarifa de $5")
+            self.balance -= 5
+        return self
+
+    #Agrega un método mostrar_info_cuenta a la clase CuentaBancaria
+    def mostrar_info_cuenta (self):
+        print(f"Balance: {self.balance}")
+        return self
+
+    #Agrega un método generar_interés a la clase CuentaBancaria
+    def generar_intereses(self):
+        if self.balance>0:
+            self.balance += self.balance*self.tasa_interes
+        return self
+    
+    #BONUS NINJA: utiliza un método de clase para imprimir todas las instancias de la información de una cuenta bancaria
+    @classmethod
+    def todos_los_balances(cls):
+        sum = 0
+        # utilizamos cls para referirnos a la clase 
+        for cuenta in cls.todas_las_cuentas:
+            sum += cuenta.balance
+        return sum
---
+++ test_cuenta.py
@@ -0,0 +1,17 @@
+from cuenta import CuentaBancaria 
+
+#INSTRUCCIONES
+#Crea 2 cuentas
+#Para la primera cuenta, haz 3 depósitos y 1 retiro, luego genera intereses y muestra la información de la cuenta, todo en una línea de código (es decir, encadenando)
+#Para la segunda cuenta, haz 2 depósitos y 4 retiros, luego genera intereses y muestra la información de la cuenta, todo en una línea de código (es decir, encadenando)
+
+cuentaCarolina = CuentaBancaria(500)
+#cuentaCarolina.mostrar_info_cuenta() #comprobar que todo esta OK
+cuentaCarolina.deposito(200).deposito(100).deposito(400).retiro(600).generar_intereses().mostrar_info_cuenta()
+
+cuentaAndrea = CuentaBancaria()
+#cuentaAndrea.mostrar_info_cuenta()#comprobar que todo esta OK
+
+cuentaAndrea.deposito(1000).deposito(500).retiro(50).retiro(100).retiro(250).retiro(200).generar_intereses().mostrar_info_cuenta()
+
+print(CuentaBancaria.todos_los_balances())
